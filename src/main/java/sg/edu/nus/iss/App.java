dce907701ebd88015d0d0db4f08360f281eec804
LeeMeiLing <leem0060@outlook.com>
2023-01-19 22:56:11 +0800

    writing int to file using DataOutputStream

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,8 +1,12 @@
 package sg.edu.nus.iss;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public final class App {
@@ -36,9 +40,13 @@ public final class App {
 
         // StackExample.example();
 
+
+
         String dirPath = ".\\data";
-        //String fileName = "data.txt";
-        String fileName = "csvEmployeeFile.txt";
+        String fileName = "data.txt";
+
+        ///// for CSVWriter ///////
+        // String fileName = "csvEmployeeFile.txt"; 
 
         // // creating directory
         File newDir = new File (dirPath);
@@ -61,48 +69,69 @@ public final class App {
             System.out.println("Directory " + fileName + " already exists");
         }
 
-        // // list all files under a directory
-        // File[] fileList = newDir.listFiles();
-        // for (File f : fileList){
-        //     System.out.println("File " + f.getPath() + " : " + f.getCanonicalFile());
-        // }
+        // // list all files under a directory //
+        File[] fileList = newDir.listFiles();
+        for (File f : fileList){
+            System.out.println("File " + f.getPath() + " : " + f.getCanonicalFile());
+        }
         
-        // try(FileOutputStream fos = new FileOutputStream(newFile, true);){
+        // ///// using FOS & DOS to write integer ( not human readable file)
+        try(FileOutputStream fos = new FileOutputStream(newFile,true);){
 
         //     // for (int i = 0; i < 10; i++){
         //     //     fos.write(i);
         //     //     fos.flush();
         //     // }
 
-        //     // DataOutputStream dos = new DataOutputStream(fos);
-        //     // for (int i = 0; i < 10; i++){
-        //     //     dos.writeInt(i);
-        //     //     dos.writeChar('\n');
-        //     //     dos.flush();
-        //     // }
-
+        //     DataOutputStream dos = new DataOutputStream(fos);
         //     for (int i = 0; i < 10; i++){
-        //         char[] str = Integer.toString(i).toCharArray();
-        //         fos.write(str[0]); //only write first digit, does not work for 2 digit int
-        //         fos.write('\n');
-        //         fos.flush();
+        //         dos.writeInt(i);
+        //         dos.flush();
         //     }
 
-        //     // writing string using file output stream, need to convert to bytes before writing
-        //     String welcomeMessage= "WELCOME TO TFIP PROGRAMME";
-        //     byte[] outputData = welcomeMessage.getBytes();
-        //     fos.write(outputData);
-        //     fos.flush();
+        //     try(FileInputStream fis = new FileInputStream(newFile);){
 
-        // }
+        //         // for (int i = 0; i < 10; i++){
+        //         //     fos.write(i);
+        //         //     fos.flush();
+        //         // }
+    
+        //         DataInputStream dis = new DataInputStream(fis);
+        //         Integer[] intArr = new Integer[10];
 
-        CSVWriter csvwriter = new CSVWriter();
-        List<Employee> employees = csvwriter.generateEmployees();
-        csvwriter.writeToCSV(employees, newFile.getPath());
+        //         for(int j = 0; j < 10; j++)
+        //         {
+        //             intArr[j]= dis.readInt();
+        //         }
+                
+        //         System.out.println(Arrays.toString(intArr));
+        //         }
 
-        CSVReader csvreader = new CSVReader();
-        List<Employee> readFromFile = csvreader.readfromCSV(newFile);
-        System.out.print(readFromFile);
+            for (int i = 0; i < 10; i++){
+                char[] str = Integer.toString(i).toCharArray();
+                fos.write(str[0]); //only write first digit, does not work for 2 digit int
+                fos.write('\n');
+                fos.flush();
+            }
+
+            // writing string using file output stream, need to convert to bytes before writing
+            String welcomeMessage= "WELCOME TO TFIP PROGRAMME";
+            byte[] outputData = welcomeMessage.getBytes();
+            fos.write(outputData);
+            fos.flush();
+
+        }
+
+
+
+        ////////////// for CSVWRiter & CSVReader////////////
+        // CSVWriter csvwriter = new CSVWriter();
+        // List<Employee> employees = csvwriter.generateEmployees();
+        // csvwriter.writeToCSV(employees, newFile.getPath());
+
+        // CSVReader csvreader = new CSVReader();
+        // List<Employee> readFromFile = csvreader.readfromCSV(newFile);
+        // System.out.print(readFromFile);
 
     }
 
